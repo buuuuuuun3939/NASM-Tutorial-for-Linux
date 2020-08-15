caa2f15e2c00d13e222a459cfe5f5b31a132d77f
buuuuuuun3939 <buuuun3939@gmail.com>
2020-08-15 18:51:14 +0900

    add Command Line Arguments

---
+++ Command Line Arguments/echo.asm
@@ -0,0 +1,24 @@
+; echo.asm
+; nasm -felf64 echo.asm && gcc echo.o && ./a.out dog 22 -zzz "hi there"
+
+        global  main
+        extern  puts
+        section .text
+
+main:
+        push    rdi         ; save registers that puts uses
+        push    rsi
+        sub     rsp, 8      ; must align stack before call
+
+        mov     rdi, [rsi]  ; the argument string to display
+        call    puts wrt ..plt ; print it
+
+        add     rsp, 8      ; restore %rsp to pre-aligned value
+        pop     rsi         ; restore registers puts used
+        pop     rdi
+
+        add     rsi, 8      ; point to next argument
+        dec     rdi         ; count down
+        jnz     main        ; if not done counting keep going
+
+        ret
