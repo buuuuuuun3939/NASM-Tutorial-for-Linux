eba76f0211b89a100d24e6560ee50734f3df88d1
buuuuuuun3939 <buuuun3939@gmail.com>
2020-08-16 12:07:42 +0900

    add Floating Point Instructions

---
+++ Floating Point Instructions/sum.asm
@@ -0,0 +1,19 @@
+; sum.asm
+; nasm -felf64 sum.asm && gcc sum.o callsum.c && ./a.out
+
+; double sum(double[] array, unit64_t length)
+
+        global  sum
+
+        section .text
+sum:
+        xorpd   xmm0, xmm0    ; initialize the sum to 0
+        cmp     rsi, 0        ; special case for length = 0
+        je      done
+next:
+        addsd   xmm0, [rdi]   ; add in the current array element 
+        add     rdi, 8        ; move to next array element
+        dec     rsi           ; count down
+        jnz     next          ; if not done counting, continue
+done:
+        ret                   ; return value already in xmm0
