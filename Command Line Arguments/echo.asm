ebc1037b5534d9b41e8f71632f6676b49ecda07d
buuuuuuun3939 <buuuun3939@gmail.com>
2020-08-19 23:27:28 +0900

    updated echo.asm
    add a new comment.

--- Command Line Arguments/echo.asm
+++ Command Line Arguments/echo.asm
@@ -1,6 +1,8 @@
 ; echo.asm
 ; nasm -felf64 echo.asm && gcc echo.o && ./a.out dog 22 -zzz "hi there"
 
+; int main(int argc, char** argv)
+
         global  main
         extern  puts
         section .text
